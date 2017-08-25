f479341a035fd1978f338c08ff0245ff7dac2550
houqhlab <fz20100109@163.com>
2017-08-25 16:43:18 +0800

    add part of serial port

---
+++ robotCTedu/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+#define	COMM	L"Com3"
+
+int main()
+{
+	// create the handle of serial port
+	HANDLE hComm = INVALID_HANDLE_VALUE;
+	hComm = CreateFile(COMM, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	
+	// check the connection of serial port
+	if (hComm == INVALID_HANDLE_VALUE)
+	{
+		int errorCode = GetLastError();
+		cout << "Open " << COMM << " failed with error " << errorCode << "." << endl;
+		return 1;
+	}
+
+	// configure the serial port
+	SetupComm(hComm, 1024, 1024);
+	DCB dcb;
+	GetCommState(hComm, &dcb);
+	//dcb.BaudRate = CBR_115200;
+	//dcb.ByteSize = 8;
+	//dcb.Parity = NOPARITY;
+	//dcb.StopBits = ONESTOPBIT;
+	SetCommState(hComm, &dcb);
+
+	return 0;
+}
