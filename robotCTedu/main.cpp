9ba3a9db60372e48841872b590b297be93c134f9
houqhlab <fz20100109@163.com>
2017-09-05 20:25:01 +0800

    pose expression by both Ruler angle & Quaternion

--- robotCTedu/main.cpp
+++ robotCTedu/main.cpp
@@ -3,35 +3,73 @@
 
 using namespace std;
 
-#define PI 3.14159265358979323846
+#define PI		3.14159265358979323846
 
-typedef struct __POSE__
+// coordinate of the origin of base coordinate system
+#define BCS_X	500
+#define BCS_Y	0
+#define BCS_Z	500
+
+typedef struct __POSE_R__
 {
 	double x;
 	double y;
 	double z;
+	double Rx;
+	double Ry;
+	double Rz;
+} POSE_R;
+
+typedef struct __POSE_Q__
+{
+	double x;
+	double y;
+	double z;
+	double q0;
 	double q1;
 	double q2;
 	double q3;
-	double q4;
-} POSE;
+} POSE_Q;
 
 
-POSE* getPoses(double r, double alpha)
+POSE_Q* getPoses(double r, double alpha)
 {
 	int number = int(180 / alpha);
-	POSE* poses = new POSE[number + 1];
+	POSE_R* poses_r = new POSE_R[number + 1];
+	POSE_Q* poses_q = new POSE_Q[number + 1];
+
+	// calculate coordinate of every point during the scanning
 	for (int i = 0; i <= number; i++)
 	{
-		poses[i].x = 0 - r * sin(i * alpha * PI / 180);
-		poses[i].y = r * cos(i * alpha * PI / 180);
-		poses[i].z = 0;
-		// q1, q2, q3, q4...
+		poses_r[i].x = 0 - r * sin(i * alpha * PI / 180);
+		poses_r[i].y = 0 - r * cos(i * alpha * PI / 180);
+		poses_r[i].z = 0;
+		poses_r[i].Rx = 0;
+		poses_r[i].Ry = 0;
+		poses_r[i].Rz = (90 - i * alpha) * PI / 180;
+		cout << poses_r[i].x << "	" << poses_r[i].y << "	" << poses_r[i].Rz << endl;
 	}
 
-	return poses;
-}
+	// transfer pose variable from Ruler angle to Quaternion
+	for (int i = 0; i <= number; i++)
+	{
+		poses_q[i].x = BCS_X + poses_r[i].x;
+		poses_q[i].y = BCS_Y + poses_r[i].y;
+		poses_q[i].z = BCS_Z + poses_r[i].z;
+		poses_q[i].q0 = cos(poses_r[i].Rx / 2) * cos(poses_r[i].Ry / 2) * cos(poses_r[i].Rz / 2)
+			+ sin(poses_r[i].Rx / 2) * sin(poses_r[i].Ry / 2) * sin(poses_r[i].Rz / 2);
+		poses_q[i].q1 = sin(poses_r[i].Rx / 2) * cos(poses_r[i].Ry / 2) * cos(poses_r[i].Rz / 2)
+			- cos(poses_r[i].Rx / 2) * sin(poses_r[i].Ry / 2) * sin(poses_r[i].Rz / 2);
+		poses_q[i].q2 = cos(poses_r[i].Rx / 2) * sin(poses_r[i].Ry / 2) * cos(poses_r[i].Rz / 2)
+			+ sin(poses_r[i].Rx / 2) * cos(poses_r[i].Ry / 2) * sin(poses_r[i].Rz / 2);
+		poses_q[i].q3 = cos(poses_r[i].Rx / 2) * cos(poses_r[i].Ry / 2) * sin(poses_r[i].Rz / 2)
+			- sin(poses_r[i].Rx / 2) * sin(poses_r[i].Ry / 2) * cos(poses_r[i].Rz / 2);
+		cout << poses_q[i].q0 << "	" << poses_q[i].q1 << "	" << poses_q[i].q2 << "	" << poses_q[i].q3
+			<< "	" << poses_q[i].q0 * poses_q[i].q0 + poses_q[i].q1 * poses_q[i].q1 + poses_q[i].q2 * poses_q[i].q2 + poses_q[i].q3 * poses_q[i].q3 << endl;
+	}
 
+	return poses_q;
+}
 
 void CALLBACK TimeProc(HWND hwnd, UINT message, UINT idTimer, DWORD dwTime)
 {
@@ -44,11 +82,8 @@ int main()
 	double angle = 15;
 
 	// calculate the poses moved during the scanning
-	POSE* poses = getPoses(radium, angle);
-	for (int i = 0; i <= 180 / angle; i++)
-	{
-		cout << poses[i].x << "	" << poses[i].y << endl;
-	}
+	POSE_Q* poses = getPoses(radium, angle);
+	
 
 	// 
 
