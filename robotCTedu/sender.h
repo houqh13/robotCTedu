076135e1847802080d83a71e13607838de096cb7
houqhlab <fz20100109@163.com>
2017-09-09 16:28:42 +0800

    socket complete

--- robotCTedu/sender.h
+++ robotCTedu/sender.h
@@ -14,17 +14,21 @@ public:
 	bool socketSetup();
 	bool socketAccept();
 	void serialRcv();
-	int socketRcv(int i);
+	void serialSend(int i);
+	void socketSend(double* data);
 	bool isAllReached();
 	void serialClose();
 	void socketClose();
+
+public:
+	int double2String(double* d, char* str, int prec);
 
 private:
 	HANDLE hComm;
 	SOCKET server;
 	SOCKET client[2];
 	char recvData[256];
-	char sendData[256];
+	char* sendData;
 	bool isSerialReached[2];
 	bool isSocketReached[2];
 };
