1013b7fb1c51715d4edc0a9c633fa15d391f2f58
houqhlab <fz20100109@163.com>
2018-09-25 16:57:55 +0800

    blank MFC application

--- README.md
+++
@@ -1,2 +0,0 @@
-# robotCTedu
-Educational platform of robot CT system on industrial manipulator Rokae XB4, developed by VC++.
---
+++ robotCT/robotCT.h
@@ -0,0 +1,32 @@
+
+// robotCT.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CrobotCTApp:
+// 有关此类的实现，请参阅 robotCT.cpp
+//
+
+class CrobotCTApp : public CWinApp
+{
+public:
+	CrobotCTApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CrobotCTApp theApp;
